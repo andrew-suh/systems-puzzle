d21df5da7c31c274ce17252b47287771cb769a3c
github_user <andrew.suh.94@gmail.com>
2019-08-21 23:04:32 -0700

    Added Documentation on changes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-import logging
 
 from flask import Flask, render_template, redirect, url_for, flash
 from forms import ItemForm
@@ -14,9 +13,9 @@ app.secret_key = os.environ['APP_SECRET_KEY']
 @app.route("/", methods=('GET', 'POST'))
 def add_item():
     form = ItemForm()
-    #when posting
+    # Only when posting
     if form.validate_on_submit():
-        #populate item for insertion
+        # Populate item for insertion
         item = Items(name=form.name.data, quantity=form.quantity.data,
                      description=form.description.data, date_added=datetime.datetime.now())
         try:
@@ -24,7 +23,7 @@ def add_item():
             db_session.commit()
             return redirect(url_for('success'))
         except:
-            #When failed, presing the back key will keep the input boxes filled
+            #When failed, pressing the back key will keep the input boxes filled
             return "Go back and check if quantity is a number or not."
     return render_template('index.html', form=form)
 
@@ -33,20 +32,20 @@ def add_item():
 def success():
     results = []
 
-    qry = db_session.query(Items)
+    qry = db_session.query(Items).order_by("id")
     results = qry.all()
 
     return render_template('get_all.html', inspect=results)
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    #Query based on the id passed
+    # Query based on the id passed
     to_delete = Items.query.filter_by(id=id).first()
     try:
-        #delete instead of add
+        # Delete instead of add
         db_session.delete(to_delete)
         db_session.commit()
-        #show all again
+        # Show all again
         return redirect(url_for('success'))
     except:
         return "Failed to Delete"
@@ -65,7 +64,8 @@ def update(id):
                 elif k is 'id':
                     pass
                 else:
-                    setattr(to_change,k, getattr(form,k).data)
+                    # Appending keys to variables break syntax
+                    setattr(to_change, k, getattr(form,k).data)
             db_session.commit()
             return redirect(url_for('success'))
         except:
@@ -73,4 +73,5 @@ def update(id):
     return render_template('update.html', form=form)
 
 if __name__ == '__main__':
+    # Added port to make other configs work
     app.run(host='0.0.0.0', port=5001)
